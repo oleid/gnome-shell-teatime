6cac4a1ba672f69872b58c5219792f45ee7ff693
Olaf Leidinger <oleid@mescharet.de>
2013-10-13 12:12:34 +0200

    added utils.js and prefs.js to pack script

--- pack.sh
+++ pack.sh
@@ -4,4 +4,4 @@
 
 today=$(date +"%Y%m%d")
 
-zip TeaTime@oleid.mescharet.de-$today.zip *.md extension.js install.sh *.svg metadata.json schemas/org.gnome.shell.extensions.teatime.gschema.xml schemas/gschemas.compiled
+zip TeaTime@oleid.mescharet.de-$today.zip *.md utils.js prefs.js extension.js install.sh *.svg metadata.json schemas/org.gnome.shell.extensions.teatime.gschema.xml schemas/gschemas.compiled
