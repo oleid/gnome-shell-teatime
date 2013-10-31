83c4bcd90e84759cdbe82eb6e28b4b9df5dc1264
Olaf Leidinger <oleid@mescharet.de>
2013-10-31 10:16:21 +0100

    clean-up for new configure system

--- pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# pack for upload to extensions.gnome.org
-
-today=$(date +"%Y%m%d")
-
-zip TeaTime@oleid.mescharet.de-$today.zip *.md utils.js prefs.js extension.js install.sh *.svg metadata.json schemas/org.gnome.shell.extensions.teatime.gschema.xml schemas/gschemas.compiled
