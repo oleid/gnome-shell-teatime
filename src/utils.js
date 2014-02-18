fe5b801203d697d9d3b81574957eb16f9e4d074c
Olaf Leidinger <oleid@mescharet.de>
2014-02-19 00:36:14 +0100

    remove display of fractional seconds

--- src/utils.js
+++ src/utils.js
@@ -71,7 +71,7 @@ function formatTime(sec_num) {
 
     let hours   = Math.floor(sec_num / 3600);
     let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    let seconds = sec_num - (hours * 3600) - (minutes * 60);
+    let seconds = Math.round(sec_num - (hours * 3600) - (minutes * 60));
 
     if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
