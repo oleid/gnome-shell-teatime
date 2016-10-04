171b506c92e98b11f72651176849f50f2630c60a
Olaf Leidinger <oleid@mescharet.de>
2016-10-04 22:12:49 +0200

    automatic merge of new translations

--- po/update_strings.sh
+++ po/update_strings.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 SCRIPTDIR=`dirname $0`
 xgettext  --from-code=UTF-8 -k_ -kN_  -o TeaTime.pot "$SCRIPTDIR"/../src/*.js "$SCRIPTDIR"/../src/schemas/*.xml
+
+for fn in *.po; do
+	msgmerge -U "$fn" TeaTime.pot
+done
