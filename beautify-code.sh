9d1e631ec24d4bea8f9ff0eb1e54278666d22fa6
Olaf Leidinger <oleid@mescharet.de>
2021-09-12 19:30:38 +0200

    FEATURE: Use long options

--- beautify-code.sh
+++ beautify-code.sh
@@ -4,5 +4,9 @@ SRC_DIR=$(dirname $0)
 echo $SRC_DIR
 
 for fn in "${SRC_DIR}"/src/*.js ; do
-	js-beautify -t -n -r -j $fn
+	js-beautify --replace \
+		--indent-with-tabs \
+		--end-with-newline \
+		--jslint-happy \
+		$fn
 done
